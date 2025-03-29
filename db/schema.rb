48b6528a5b74fe5c96091f30dc6d02c1f24da538
Vlad Lozynskyi <vladloz@hotmail.com>
2025-03-29 16:44:57 +0200

    Upgrade project to Rails 8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_06_083919) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_05_105620) do
   create_table "applications", force: :cascade do |t|
     t.integer "edebo_person_card", null: false
     t.string "fullname_birthdate", null: false
@@ -42,5 +42,4 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_06_083919) do
     t.datetime "updated_at", null: false
     t.index ["edebo_study_card"], name: "index_students_on_edebo_study_card", unique: true
   end
-
 end
